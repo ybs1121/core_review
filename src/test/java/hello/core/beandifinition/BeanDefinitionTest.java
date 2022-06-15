7f7a1941ac61ea36a702df1e6a2536ca5c4d28cd
yangbyoungseok <ybs991@naver.com>
2022-06-15 20:03:00 +0900

    스프링 컨테이너와 스프링 빈

---
+++ src/test/java/hello/core/beandifinition/BeanDefinitionTest.java
@@ -0,0 +1,25 @@
+package hello.core.beandifinition;
+
+import hello.core.AppConfig;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.config.BeanDefinition;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+public class BeanDefinitionTest {
+
+    AnnotationConfigApplicationContext ac = new AnnotationConfigApplicationContext(AppConfig.class);
+
+    @Test
+    void findApplicationBean(){
+        String[] beanDefinitionNames = ac.getBeanDefinitionNames();
+        for (String beanDefinitionName : beanDefinitionNames) {
+            BeanDefinition beanDefinition = ac.getBeanDefinition(beanDefinitionName);
+
+            if(beanDefinition.getRole() == BeanDefinition.ROLE_APPLICATION){
+                System.out.println("beanDefinitionName = " + beanDefinitionName + " beanDefinition  = "
+                + beanDefinition
+                );
+            }
+        }
+    }
+}
