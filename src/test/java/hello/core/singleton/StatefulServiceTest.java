3a60e2405ccb8b272900036cbd590b0b510255be
yangbyoungseok <ybs991@naver.com>
2022-06-16 15:18:17 +0900

    싱글톤 컨터이너

---
+++ src/test/java/hello/core/singleton/StatefulServiceTest.java
@@ -0,0 +1,38 @@
+package hello.core.singleton;
+
+import ch.qos.logback.core.rolling.TimeBasedFileNamingAndTriggeringPolicy;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.Bean;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class StatefulServiceTest {
+
+    @Test
+    void statefulServiceSingleton(){
+        ApplicationContext ac = new AnnotationConfigApplicationContext(TestConfig.class);
+        StatefulService statefulService1 = ac.getBean(StatefulService.class);
+        StatefulService statefulService2 = ac.getBean(StatefulService.class);
+
+        //ThreadA: UesrA orders 10000won
+        statefulService1.order("userA",10000);
+        //ThreadB: UserB orders 20000won
+        statefulService2.order("userB", 20000);
+
+        int price = statefulService1.getPrice();
+        System.out.println("statefulService1 = " + price);
+
+        Assertions.assertThat(statefulService1.getPrice()).isEqualTo(20000);
+    }
+
+    static class TestConfig{
+        @Bean
+        public StatefulService statefulService() {
+            return new StatefulService();
+        }
+    }
+
+}
